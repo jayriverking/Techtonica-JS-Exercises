d8b76433ffe66ecd2f687a485616ec3d9ef81810
Jay King <jayriverking@gmail.com>
2022-12-12 16:23:23 -0800

    finished 7_functionsChallenge.js

--- prework/7_FunctionsChallenges.js
+++ prework/7_FunctionsChallenges.js
@@ -98,22 +98,60 @@ console.log(removeFromString('Hello School', 6, 400)); // --> 'Hello '
 // Exercise 5. Write a function called indexOf, which accepts an array and a number.
 // The function should return the first index at which the value exists or -1 if the value is not found.
 // IMPORTANT: Do not use the built in array.indexOf() function!
+
+function indexOf (arr, num){
+    //go through array to find the number - using for loop
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === num){
+            return i;
+        }
+    }
+    //if it doesn't exist, return -1
+    return -1;
+}
+
 // Examples:
-// let arr = [5, 10, 15, 20];
-// indexOf(arr, 20); // --> 3
-// let arr2 = [1, 2, 3, 4, 5];
-// indexOf(arr2, 2); // --> 1
-// let arr3 = [1, 2];
-// indexOf(arr3, 10); // --> -1
+let arr = [5, 10, 15, 20];
+console.log(indexOf(arr, 20)); // --> 3
+let arr2 = [1, 2, 3, 4, 5];
+console.log(indexOf(arr2, 2)); // --> 1
+let arr3 = [1, 2];
+console.log(indexOf(arr3, 10)); // --> -1
+
+
 
 // Exercise 6. Write a function called includes which accepts a collection, a value, and an optional starting index. The function should return true if the value exists in the collection when we search starting from the starting index. Otherwise, it should return false.
 // The collection can be a string, an array, or an object. If the collection is a string or array, the third parameter is a starting index for where to search from. If the collection is an object, the function searches for the value among values in the object; since objects have no sort order, the third parameter is ignored.
-// Examples:
-// includes([1, 2, 3], 1) // --> true
-// includes([1, 2, 3], 1, 2) // --> false
-// includes([1, 2, 3], 6) // --> false
-// includes({ 'a': 1, 'b': 2 }, 1) // --> true
-// includes({ 'a': 1, 'b': 2 }, 'a') // --> false
-// includes('abcd', 'b') // --> true
-// includes('abcd', 'e') // --> false
-// includes('abcd', 'a', 2) // --> false
+
+function includes(collection, value, index = 0) {
+    //if string or array - iterate through with a for loop
+    if (typeof collection === 'string' || typeof collection === 'array') {
+      //for loop - starting index = index, added a default index value of 0 to the parameter
+      for (let i = index; i < collection.length; i++) {
+        if (collection[i] === value) {
+          return true;
+        }
+      }
+    }
+  
+    if (typeof collection === 'object') {
+      //using for in loop to iterate through object
+      for (let item in collection) {
+        if (collection[item] === value) {
+          return true;
+        }
+      }
+    }
+    //otherwise return false
+    return false;
+  }
+  
+  // Examples:
+  console.log(includes([1, 2, 3], 1)); // --> true
+  console.log(includes([1, 2, 3], 1, 2)); // --> false - shouldn't this be true? the value of  1 is included in [1, 2, 3]
+  console.log(includes([1, 2, 3], 6)); // --> false
+  console.log(includes({ 'a': 1, 'b': 2 }, 1)); // --> true
+  console.log(includes({ 'a': 1, 'b': 2 }, 'a')); // --> false
+  console.log(includes('abcd', 'b')); // --> true
+  console.log(includes('abcd', 'e')); // --> false
+  console.log(includes('abcd', 'a', 2)); // --> false
