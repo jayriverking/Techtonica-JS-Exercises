78c0cbf2ee9d9c803484e70f01e1c5092a5d954b
Jay King <jayriverking@gmail.com>
2022-12-13 10:50:01 -0800

    fixed last exercise

--- prework/7_FunctionsChallenges.js
+++ prework/7_FunctionsChallenges.js
@@ -123,35 +123,41 @@ console.log(indexOf(arr3, 10)); // --> -1
 // Exercise 6. Write a function called includes which accepts a collection, a value, and an optional starting index. The function should return true if the value exists in the collection when we search starting from the starting index. Otherwise, it should return false.
 // The collection can be a string, an array, or an object. If the collection is a string or array, the third parameter is a starting index for where to search from. If the collection is an object, the function searches for the value among values in the object; since objects have no sort order, the third parameter is ignored.
 
-function includes(collection, value, index = 0) {
-    //if string or array - iterate through with a for loop
-    if (typeof collection === 'string' || typeof collection === 'array') {
-      //for loop - starting index = index, added a default index value of 0 to the parameter
-      for (let i = index; i < collection.length; i++) {
-        if (collection[i] === value) {
-          return true;
-        }
-      }
-    }
-  
-    if (typeof collection === 'object') {
-      //using for in loop to iterate through object
-      for (let item in collection) {
-        if (collection[item] === value) {
-          return true;
-        }
-      }
-    }
-    //otherwise return false
-    return false;
+function includes(collection, value, index) {
+    
+  // if no index provided, make it 0
+  if (!index) {
+    index = 0;
   }
-  
-  // Examples:
-  console.log(includes([1, 2, 3], 1)); // --> true
-  console.log(includes([1, 2, 3], 1, 2)); // --> false - shouldn't this be true? the value of  1 is included in [1, 2, 3]
-  console.log(includes([1, 2, 3], 6)); // --> false
-  console.log(includes({ 'a': 1, 'b': 2 }, 1)); // --> true
-  console.log(includes({ 'a': 1, 'b': 2 }, 'a')); // --> false
-  console.log(includes('abcd', 'b')); // --> true
-  console.log(includes('abcd', 'e')); // --> false
-  console.log(includes('abcd', 'a', 2)); // --> false
+  //if string or array - iterate through with a for loop
+  if (typeof collection === 'string' || typeof collection === 'array') {
+    //for loop - starting index = index
+    for (let i = index; i < collection.length; i++) {
+
+      if (collection[i] === value) {
+        return true;
+      }
+    }
+  }
+
+  if (typeof collection === 'object') {
+    //using for in loop to iterate through object
+    for (let item in collection) {
+      if (collection[item] === value) {
+        return true;
+      }
+    }
+  }
+  //otherwise return false
+  return false;
+}
+
+// Examples:
+console.log(includes([1, 2, 3], 1)); // --> true
+console.log(includes([1, 2, 3], 1, 2)); // --> false - this returns "true" for me even though the last test case gives me a "false" and I don't know why
+console.log(includes([1, 2, 3], 6)); // --> false
+console.log(includes({ 'a': 1, 'b': 2 }, 1)); // --> true
+console.log(includes({ 'a': 1, 'b': 2 }, 'a')); // --> false
+console.log(includes('abcd', 'b')); // --> true
+console.log(includes('abcd', 'e')); // --> false
+console.log(includes('abcd', 'a', 2)); // --> false
